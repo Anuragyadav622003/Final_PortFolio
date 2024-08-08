a90e43a3ab878464d78600399a1be024791c4a29
Anurag yadav <anuragyadav622003@gmail.com>
2024-08-08 13:39:05 +0530

    changes

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import { useState } from 'react';
 import Home from '../../pages/Home/Home'
 import { AiOutlineDoubleLeft ,AiOutlineDoubleRight } from "react-icons/ai";
 import './Layout.css';
 import { Menu } from '../Menu/Menu';
-import About from '../../pages/about/About';
+
 
 
 
@@ -42,49 +42,6 @@ const Layout = ()=>
         </>
     )
 }
-=======
-import React from 'react'
-import { useState } from 'react';
-import Home from '../../pages/Home/Home'
-import { AiOutlineDoubleLeft ,AiOutlineDoubleRight } from "react-icons/ai";
-import './Layout.css';
-import { Menu } from '../Menu/Menu';
-import About from '../../pages/about/About';
 
 
-
-const Layout = ()=>
-{
-    const [toggle,setToggle] = useState(false);
-
-    const handleToggle = ()=>
-    {
-        setToggle(!toggle);
-    };
-    return(
-        <>
-          
-          <div className='sidebar-section'>
-            <div className={toggle?("sidebar"):("sidebar-toggle sidebar")}>
-                <div className='sidebar-toggle-icons'>
-                    
-                    <p onClick={handleToggle}>
-                        {
-                            toggle?( <AiOutlineDoubleRight size={20}/>):( <AiOutlineDoubleLeft size={20}/>)
-                        }
-                    
-                    </p>
-                   
-                </div>
-                <Menu toggle={toggle} />
-            </div>
-            <div className='container'>
-               <Home/>
-            </div>
-          </div>
-
-        </>
-    )
-}
->>>>>>> f57e0b7053f52af1a278a90936421293ec63a9e1
 export default Layout;
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState, createContext, useContext } from "react";
 
 const ThemeContext = createContext();
@@ -17,23 +16,3 @@ const ThemeProvider = ({ children }) => {
 const useTheme = () => useContext(ThemeContext);
 
 export { useTheme, ThemeProvider };
-=======
-import { useState, createContext, useContext } from "react";
-
-const ThemeContext = createContext();
-
-const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
-
-  return (
-    <ThemeContext.Provider value={[theme, setTheme]}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
-
-//custom hook
-const useTheme = () => useContext(ThemeContext);
-
-export { useTheme, ThemeProvider };
->>>>>>> f57e0b7053f52af1a278a90936421293ec63a9e1
